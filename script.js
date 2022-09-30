6aca0764e000c6d7ae918b6bc5a2cc85a0569933
nalin243 <devpriyanalin@gmail.com>
2022-09-30 22:06:29 +0530

    Added read status to the display and add book section

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ libraryForm.addEventListener('submit', (e) => {
 
     let name = document.getElementById("bookName").value;
     let author = document.getElementById("author").value;
+    let read = document.getElementById("read").checked;
     let type;
 
     let fiction = document.getElementById('fiction');
@@ -64,14 +65,16 @@ libraryForm.addEventListener('submit', (e) => {
             myObj = {
                 book: name,
                 bookauthor: author,
-                bookType: type
+                bookType: type,
+                readStatus: read
             }
         }
         else{ // Book Author not entered then set it to Unknown
             myObj = {
                 book: name,
                 bookauthor: "Unknown",
-                bookType: type
+                bookType: type,
+                readStatus: read
             }
         }
         objOfBook.push(myObj);
@@ -114,6 +117,7 @@ function displayBooks() {
            <td class="name">${books.book}</td>
            <td class="author">${books.bookauthor}</td>
            <td class="type">${books.bookType}</td>
+           <td class="type">${books.readStatus ? "Yes" : "No"}</td>
            <td class="icon"><i class="fa fa-times" aria-hidden="true" onclick="removeBook(${index})"></i></td>
            </tr>
         `;
@@ -125,6 +129,7 @@ function displayBooks() {
            <td class="name">${books.book}</td>
            <td class="author">${books.bookauthor}</td>
            <td class="type">${books.bookType}</td>
+           <td class="type">${books.readStatus ? "Yes" : "No"}</td>
            <td class="icon"><i class="fa fa-times" aria-hidden="true" onclick="removeBook(${index})"></i></td>
            </tr>
         `;
