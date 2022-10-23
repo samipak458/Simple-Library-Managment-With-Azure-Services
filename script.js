87178289995b363256381f99bbee864636aab56d
Shaba1406 <shabazehra7711@gmail.com>
2022-10-23 16:40:14 +0530

    FAQ Section

--- script.js
+++ script.js
@@ -628,3 +628,17 @@ icon.onclick = ()=>{
   }
   console.log(document.body.classList);
 }
+var acc = document.getElementsByClassName("accordion");
+var i;
+var len = acc.length;
+for (i = 0; i < len; i++) {
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
